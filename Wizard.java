62d1be40eacd2bb873fc3184eece2cc2d4849051
mzqan <123510256+mzqan@users.noreply.github.com>
2023-05-29 23:45:08 -0400

    Errors with Finding Images

--- Wizard.java
+++ Wizard.java
@@ -3,17 +3,31 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Wizard here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Megan
+ * @version June 2023
  */
 public class Wizard extends Actor
 {
-    /**
-     * Act - do whatever the Wizard wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    GreenfootImage idle = new GreenfootImage("images/wizard_sprites/wizard_attack1.png");
+    //Array for sprites
+    GreenfootImage[] attack = new GreenfootImage[15];
+    
+    public Wizard() {
+        for(int i = 2; i < 17;i++) {
+            attack[i] = new GreenfootImage("images/wizard_sprites/wizard_attack" + i + ".png");
+        }
+    }
     public void act()
     {
-        // Add your action code here.
+        setRotation(270);
+        
+        if(Greenfoot.isKeyDown("up"))
+        {
+            move(-3);
+        }
+        else if(Greenfoot.isKeyDown("down"))
+        {
+            move(3);
+        }
     }
 }
