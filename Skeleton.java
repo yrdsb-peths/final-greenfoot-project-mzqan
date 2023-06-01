5dcb9300b3c1238ecc473ae71d56462de4a7aae2
mzqan <123510256+mzqan@users.noreply.github.com>
2023-06-01 16:58:55 -0400

    Added Wand.java - follows cursor

--- Skeleton.java
+++ Skeleton.java
@@ -1,15 +1,14 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Danger for our Wizard.
+ * Danger for our wizard.
  * 
- * @author Megan Lee
- * @version June 2023
+ * @author (your name) 
+ * @version (a version number or a date)
  */
 public class Skeleton extends Actor
 {
     GreenfootImage skull = new GreenfootImage("images/skull.png");
-    
     /**
      * Act - do whatever the Skeleton wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
