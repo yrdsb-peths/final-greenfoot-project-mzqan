8ce9b4d96835a0111f8b275fc34bad7e9226b606
mzqan <123510256+mzqan@users.noreply.github.com>
2023-06-02 16:11:07 -0400

    Updated Skeleton.java - extends SmoothMover, "falls" randomly

--- Skeleton.java
+++ Skeleton.java
@@ -3,19 +3,31 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Danger for our wizard.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Megan 
+ * @version June 2023
  */
-public class Skeleton extends Actor
+public class Skeleton extends SmoothMover
 {
     GreenfootImage skull = new GreenfootImage("images/skull.png");
     /**
      * Act - do whatever the Skeleton wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act()
-    {
+    public Skeleton() {
         setImage(skull);
         skull.scale(80,80);
     }
+    public void act(){
+        //Apple falls downwards
+        int x = getX();
+        double y = getY() + 0.5;
+        setLocation(x, y);
+        
+        GameWorld world = (GameWorld) getWorld();
+        if(getY() >= world.getHeight()) 
+        {
+            world.removeObject(this);
+        }
+    }
+    
 }
