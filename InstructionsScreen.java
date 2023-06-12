eecceab92c3c1669933f99396353c1b1f5cc22c1
mzqan <123510256+mzqan@users.noreply.github.com>
2023-06-12 17:02:14 -0400

    Added NextButton.java & BackButton.java, Updated InstructionsScreen.java - How To Play Screen functionality

--- InstructionsScreen.java
+++ InstructionsScreen.java
@@ -1,14 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class InstructionsScreen here.
+ * How To Play Screen.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Megan Lee 
+ * @version June 2023
  */
 public class InstructionsScreen extends World
-{
-
+{   
+    GreenfootImage[] instructions = new GreenfootImage[3];
+    public static int currentScreen = 0;
     /**
      * Constructor for objects of class InstructionsScreen.
      * 
@@ -17,5 +18,45 @@ public class InstructionsScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        prepare();
+    }
+    
+    public void prepare() {
+        for(int i=0; i<3;i++) {
+            instructions[i] = new GreenfootImage("images/howtoplay" + (i+1) + ".png");
+            instructions[i].scale(600,400);
+        }
+        
+        BackButton backBtn = new BackButton();
+        addObject(backBtn, 185, 355);
+        
+        NextButton nextBtn = new NextButton();
+        addObject(nextBtn, 420, 355);
+        
+        setBackground(instructions[currentScreen]);
+    }
+    
+    public static void switchBack(){
+        if(currentScreen>0){
+            currentScreen--;
+        }
+        else {
+            World titlescreen = new TitleScreen();
+            Greenfoot.setWorld(titlescreen);
+        }
+    }
+    
+    public static void switchNext(){
+        if(currentScreen<2) {
+            currentScreen++;
+        }
+        else {
+            World titlescreen = new TitleScreen();
+            Greenfoot.setWorld(titlescreen);
+        }
+    }
+    
+    public void act(){
+        setBackground(instructions[currentScreen]);
     }
 }
